30790448c8e1523c81e6c238bd8af2a0ff66891d
Mehmet Oğuz Özüer <96492946+Oguzoz1@users.noreply.github.com>
2024-11-09 14:36:53 +0300

    Update PostController.java

--- Communitter/src/main/java/com/communitter/api/controller/PostController.java
+++ Communitter/src/main/java/com/communitter/api/controller/PostController.java
@@ -27,6 +27,11 @@ public class PostController {
        return ResponseEntity.ok(postService.createPost(id, post));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@P("id") @PathVariable Long id, Post post){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+    
     @PreAuthorize("@authorizer.checkSubscription(#root,#id)")
     @PostMapping("/{id}/vote")
     public ResponseEntity<PostVote> votePost(@P("id") @PathVariable Long id, @RequestParam  boolean isUpvote){

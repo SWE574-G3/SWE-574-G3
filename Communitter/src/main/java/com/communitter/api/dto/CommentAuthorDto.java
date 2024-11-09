2de586a4f1240a0e0d78fd85846f95c24f90144b
Mehmet Oğuz Özüer <96492946+Oguzoz1@users.noreply.github.com>
2024-11-09 14:40:50 +0300

    [ADD] DTO and Service update

--- Communitter/src/main/java/com/communitter/api/dto/CommentAuthorDto.java
+++ Communitter/src/main/java/com/communitter/api/dto/CommentAuthorDto.java
@@ -1,7 +1,5 @@
 package com.communitter.api.dto;
 
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,14 +9,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommentDto {
+public class CommentAuthorDto {
+    private Long id;
 
-    private Long id;    
-    
-    private CommentAuthorDto author;
-    
-    private Date date;
-
-    private String content;
-
+    private String username;
 }

8e7353f3ede8d39ae3c3140d53e218a9f0038c61
Mehmet Oğuz Özüer <96492946+Oguzoz1@users.noreply.github.com>
2024-11-23 14:05:29 +0300

    [ADD] Edit and Delete buttons

--- communitter-frontend/src/components/CommentCard.js
+++ communitter-frontend/src/components/CommentCard.js
@@ -3,30 +3,43 @@ import CardTitle from "react-bootstrap/CardTitle";
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import CardBody from "react-bootstrap/CardBody";
+import Button from "react-bootstrap/esm/Button";
 
-export const CommentCard = ({comment}) => {
-    const {author, date: timestamp, content, id} = comment;
+export const CommentCard = ({ comment }) => {
+    const { author, date: timestamp, content, id } = comment;
     const navigate = useNavigate();
 
     const directToUserView = () => {
         navigate(`/user/${author.id}`)
     };
 
+    const handleDeleteComment = () => {
+
+    };
+
     return (
         <Card className="mb-3">
-            <CardTitle onClick={directToUserView} style={{cursor: "pointer"}}>
+            <CardTitle onClick={directToUserView} style={{ cursor: "pointer" }}>
                 {author.username} - {" "}
-                {new Date(timestamp).toLocaleDateString("tr-TR",{
+                {new Date(timestamp).toLocaleDateString("tr-TR", {
                     year: "numeric",
                     month: "2-digit",
                     day: "2-digit",
                     hour: "2-digit",
-                    minute:"2-digit",
+                    minute: "2-digit",
                     hour12: false,
                 })}{" "}
             </CardTitle>
             <CardBody>
                 <p>{content}</p>
+                <div className="d-flex justify-content-between">
+                    <Button variant="primary" onClick={handleDeleteComment}>
+                        Edit
+                    </Button>
+                    <Button variant="danger" onClick={handleDeleteComment}>
+                        Delete
+                    </Button>
+                </div>
             </CardBody>
         </Card>
     )
